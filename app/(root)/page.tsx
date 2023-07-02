398e2a9125dd4f5ec3194614d285b16ce5f9e128
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-07-02 12:18:22 +0200

    create a modal template

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello Admin Dashboard</h1>
-      <UserButton afterSignOutUrl="/" />
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <Modal
+        title="test"
+        description="test-description"
+        isOpen
+        onClose={() => {}}
+      >
+        children
+      </Modal>
       <Button size="lg"> Log in</Button>
     </main>
   );
