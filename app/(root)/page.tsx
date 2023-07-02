21e40989670a9d1b3ba6a1bd2f7ecd578f1edbbc
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-07-02 11:47:10 +0200

    authentication signup, signin, user profile , logout, verfiication, and user account

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello Admin Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
       <Button size="lg"> Log in</Button>
     </main>
   );
