63d3a7fc9b4c4ec2ed3aa98ca39b56bb1b86783e
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-07-02 11:23:20 +0200

    install clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello Admin Dashboard</h1>
+      <Button size="lg"> Log in</Button>
     </main>
   );
 }
