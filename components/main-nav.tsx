5ddb8b02d0f7434b2834ce50ebdcd8e540eb50b4
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-07-04 23:50:32 +0200

    create a billboard with img and label , udpate, delete

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -18,6 +18,11 @@ const MainNav = ({
       active: pathName === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathName === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "settings",
       active: pathName === `/${params.storeId}/settings`,
